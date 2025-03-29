8e53913ab0ebbe59570e02fbb298488a9903d9a8
siddkalani <siddharthkalani6@gmail.com>
2025-03-29 15:33:10 +0530

    fix: updated start script for Elastic Beanstalk

--- frontend/constants/constant.js
+++ frontend/constants/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://crm-jmlc.onrender.com"
-// export const BASE_URL = "http://192.168.29.108:3002"
+export const BASE_URL = "http://crm-dev2.eu-north-1.elasticbeanstalk.com/";
+// export const BASE_URL = "http://192.168.29.2:3002"
